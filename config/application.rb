4830b5b4f1251df732102f902937cd79538e8898
David Bista <ytd3809@gmail.com>
2026-03-29 08:41:36 +0545

    feat: enable session store for api only application

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,10 @@ module KitabSataPata
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Enabled the session store for api_only application
+    config.session_store :cookie_store, key: "_kitab_sata_pata_session"
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
