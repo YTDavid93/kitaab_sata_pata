645995bb93369376e2f90253615565d051045574
David Bista <ytd3809@gmail.com>
2026-04-05 22:05:44 +0545

    feat: run migration for listing model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_29_161330) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_05_095130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.string "book_name", null: false
+    t.string "author_name", null: false
+    t.string "genre", null: false
+    t.string "description", null: false
+    t.string "status", default: "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
