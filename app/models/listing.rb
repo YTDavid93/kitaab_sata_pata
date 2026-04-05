f387b21dcbc7966c64cbf680cfe497a676eb519f
David Bista <ytd3809@gmail.com>
2026-04-05 22:03:22 +0545

    feat: implement Listing model and migration

---
+++ app/models/listing.rb
@@ -0,0 +1,18 @@
+class Listing < ApplicationRecord
+  belongs_to :user
+  before_create :set_default_status
+
+  validate :user_id, :title, :book_name, :author_name, :genre, :description, presence: true
+  validates :description, length: { maximum: 150 }
+
+  validates :id, uniqueness: {
+    scope: :user_id,
+    message: "You have already requested this book"
+  }
+
+  private
+
+  def set_default_status
+    self.status ||= "active"
+  end
+end
