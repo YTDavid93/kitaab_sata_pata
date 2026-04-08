45ac02a3e7929d083d5bb2c3ac3c75ca640df151
David Bista <ytd3809@gmail.com>
2026-04-08 22:33:15 +0545

    feat: add Request model and migration for managing requests

---
+++ app/models/request.rb
@@ -0,0 +1,17 @@
+class Request < ApplicationRecord
+  belongs_to :requester, class_name: "User"
+  belongs_to :requestee, class_name: "User"
+  belongs_to :listing, class_name: "Listing"
+  belongs_to :offered_listing, class_name: "Listing"
+
+  validates :requester_id, :requestee_id, :listing_id, presence: true
+  validates :status, inclusion: { in: %w[pending accepted declined], message: "%{value} is not a valid status" }, allow_nil: true
+
+  before_create :set_default_status
+
+  private
+
+  def set_default_status
+    self.status ||= "pending"
+  end
+end
---
+++ db/migrate/20260406112653_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration[8.0]
+  def change
+    create_table :requests do |t|
+      t.references :requester, null: false,  foreign_key: { to_table: :users }
+      t.references :requestee, null: false, foreign_key: { to_table: :users }
+      t.references :listing, null: false, foreign_key: { to_table: :listings }
+      t.references :offered_listing, null: false, foreign_key: { to_table: :listings }
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
