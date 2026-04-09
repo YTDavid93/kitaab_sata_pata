eb6bf52722157e3b0e13f3e5a8d5503e5ac5f375
David Bista <ytd3809@gmail.com>
2026-04-09 17:34:28 +0545

    feat: add listing_image attachment with thumbnail variant to Listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,9 @@ class Listing < ApplicationRecord
   belongs_to :user
   before_create :set_default_status
   has_many :requests, dependent: :destroy
+  has_one_attached :listing_image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 300, 200 ]
+  end
 
   validates :user_id, :title, :book_name, :author_name, :genre, :description, presence: true
   validates :description, length: { maximum: 150 }
