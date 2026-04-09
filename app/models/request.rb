5613f47e1824c5b80773dfaf78b29cac46db5535
David Bista <ytd3809@gmail.com>
2026-04-09 09:26:28 +0545

    fix: add offered_listing_id to presence validation in Request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,6 @@ class Request < ApplicationRecord
   belongs_to :listing, class_name: "Listing"
   belongs_to :offered_listing, class_name: "Listing"
 
-  validates :requester_id, :requestee_id, :listing_id, presence: true
+  validates :requester_id, :requestee_id, :listing_id, :offered_listing_id, presence: true
   validates :status, inclusion: { in: %w[pending accepted declined], message: "%{value} is not a valid status" }
 end
