0f2ada948825d5ecc9be7fc19c31fef0db9c1e50
David Bista <ytd3809@gmail.com>
2026-04-10 15:30:08 +0545

    feat: enhance NotificationChannel with user-specific streaming and cleanup on unsubscribe

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,9 +1,11 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_for current_user if current_user.present?
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
