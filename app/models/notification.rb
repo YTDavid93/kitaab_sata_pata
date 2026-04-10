c7ec1898a994518035d830b975f9f8a6ac701994
David Bista <ytd3809@gmail.com>
2026-04-10 15:38:26 +0545

    feat: add broadcast functionality to Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,17 @@ class Notification < ApplicationRecord
 
   scope :unread, -> { where(read: false) }
   scope :read, -> { where(read: true) }
+
+  def broadcast_to_user
+    ActionCable.server.broadcast(
+      "notifications_#{user_id}",
+      serialized_notification
+    )
+  end
+
+  private
+
+  def serialized_notification
+    NotificationSerializer.new(self).serializable_hash
+  end
 end
